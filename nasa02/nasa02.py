880ae4d5f594ba68f09a2354f01571e54800e0f7
coreyiscoding <coreyiscoding@gmail.com>
2020-11-11 16:12:13 +0000

    your commit message

---
+++ nasa02/nasa02.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import urllib.request
+import json
+
+## Define NEOapi 
+neourl = 'https://api.nasa.gov/neo/rest/v1/feed?'
+startdate = 'start_date=2018-06-01'
+enddate = '&end_date=END_DATE'
+mykey = '&api_key=DEMO_KEY'    ## your key goes in place of DEMO_KEY
+
+neourl = neourl + startdate + mykey
+
+## Call the webservice
+neourlobj = urllib.request.urlopen(neourl)
+
+## read the file-like object
+neoread = neourlobj.read()
+
+## decode JSON to Python data structure
+decodeneo = json.loads(neoread.decode('utf-8'))
+
+## display our Pythonic data
+print("\n\nConverted Python data")
+print(decodeneo)
+#!/usr/bin/env python3
+
+import requests ## 3rd party URL lookup
+
+## define the main function
+def main():
+    neourl = 'https://api.nasa.gov/neo/rest/v1/feed?' # API URL
+    startdate = 'start_date=2018-06-01'  ## start date for data
+    enddate = '&end_date=END_DATE' ## create a mechanism to utilize enddate
+    mykey = '&api_key=DEMO_KEY' ## replace this with our API key
+
+    neourl = neourl + startdate + mykey
+
+    neojson = (requests.get(neourl)).json()
+
+    print(neojson)
+
+## call main
+main()
+
